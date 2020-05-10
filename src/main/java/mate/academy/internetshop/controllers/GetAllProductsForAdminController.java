678eaebc386866afc9c245f4dadebdd7a56e4ee9
Anastasiia Kosiak <cctvporsh@gmail.com>
2020-05-10 20:51:40 +0300

    Added Product DAO JDBC implementation (#10)
    * Added Product DAO JDBC implementation
    
    * refactored getProductFromResultSet() method

--- src/main/java/mate/academy/internetshop/controllers/GetAllProductsForAdminController.java
+++ src/main/java/mate/academy/internetshop/controllers/GetAllProductsForAdminController.java
@@ -12,8 +12,8 @@ import mate.academy.internetshop.service.ProductService;
 
 public class GetAllProductsForAdminController extends HttpServlet {
     private static final Injector INJECTOR = Injector.getInstance("mate.academy.internetshop");
-    private final ProductService productService
-            = (ProductService)INJECTOR.getInstance(ProductService.class);
+    private final ProductService productService =
+            (ProductService)INJECTOR.getInstance(ProductService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
