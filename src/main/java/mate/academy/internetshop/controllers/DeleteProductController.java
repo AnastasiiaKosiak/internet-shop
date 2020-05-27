03d1cdcd51a65c2878977553a35c1ad5eb12d707
nastya <cctvporsh@gmail.com>
2020-05-27 22:34:06 +0300

    test

--- src/main/java/mate/academy/internetshop/controllers/DeleteProductController.java
+++ src/main/java/mate/academy/internetshop/controllers/DeleteProductController.java
@@ -5,6 +5,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.crypto.Data;
+
+import mate.academy.internetshop.exceptions.DataProcessingException;
 import mate.academy.internetshop.lib.Injector;
 import mate.academy.internetshop.service.ProductService;
 
@@ -17,7 +20,11 @@ public class DeleteProductController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         String productId = req.getParameter("id");
-        productService.delete(Long.valueOf(productId));
+        try {
+            productService.delete(Long.valueOf(productId));
+        } catch (NumberFormatException exception) {
+            throw new DataProcessingException(exception.getMessage(), exception);
+        }
         resp.sendRedirect(req.getContextPath() + "/products/admin");
     }
 }
