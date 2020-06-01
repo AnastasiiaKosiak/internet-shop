6aa1d1e566c26be4e7918936742f4d780c2ff2c4
nastya <cctvporsh@gmail.com>
2020-06-01 12:55:09 +0300

    removed unused import

--- src/main/java/mate/academy/internetshop/controllers/DeleteProductFromShoppingCartController.java
+++ src/main/java/mate/academy/internetshop/controllers/DeleteProductFromShoppingCartController.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.academy.internetshop.lib.Injector;
-import mate.academy.internetshop.model.Product;
 import mate.academy.internetshop.service.ProductService;
 import mate.academy.internetshop.service.ShoppingCartService;
 
